2bf2e5822547f16de6cf971a76753193178075ed
NottSr <leonardo.br4@hotmail.com>
2022-09-09 14:00:59 -0500

    Task 0 and 1 done

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""""""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+
+    burl = "https://www.reddit.com"
+    aurl = "{}/r/{}/about.json".format(burl, subreddit)
+
+    usag = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+            Chrome/105.0.0.0 Safari/537.36'
+    }
+
+    res = requests.get(
+        aurl,
+        headers=usag,
+        allow_redirects=False
+    )
+
+    if res.status_code in [302, 404]:
+        return 0
+
+    return res.json().get('data').get('subscribers')
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""""""
+
+import requests
+
+
+def top_ten(subreddit):
+
+    burl = "https://www.reddit.com"
+    aurl = "{}/r/{}/hot.json".format(burl, subreddit)
+
+    usag = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+            Chrome/105.0.0.0 Safari/537.36'
+    }
+
+    res = requests.get(
+        aurl,
+        headers=usag,
+        params={'limit': 10},
+        allow_redirects=False
+    )
+
+    if res.status_code in [302, 404]:
+        print(None)
+    else:
+        for pt in res.json()['data']['children']:
+            print(pt['data']['title'])
